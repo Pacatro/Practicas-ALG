f749db6151356619b9b787b2d77ac3bf1a3a94c6
Pacatro <paco.algar.15@gmail.com>
2023-09-23 13:19:59 +0200

    Apartado 1 casi acabado

--- p1/ejemplo_gnuplot.sh
+++ p1/ejemplo_gnuplot.sh
@@ -5,7 +5,7 @@ cat << _end_ | gnuplot
 set terminal postscript eps color
 set output "grafico.eps"
 set key right bottom
-set xlabel "Etiqueta eje X"
-set ylabel "Etiqueta Eje Y"
-plot 'Datos.txt' using 1:2 t "Datos columna 2 frente a 1" w l, 'Datos.txt' using 1:3 t "Datos columna 3 frente a 1" w l, 'Datos.txt' using 1:5 t "Datos columna 5 frente a 1" w l, 'Datos.txt' using 1:6 t "Datos columna 6 frente a 1" w l 
+set xlabel "Tamano ejemplar"
+set ylabel "Tiempo"
+plot './build/datosFinales.txt' using 1:2 t "Datos columna 2 frente a 1" w l, './build/datosFinales.txt' using 1:3 t "Datos columna 3 frente a 1" w l, './build/datosFinales.txt' using 1:5 t "Datos columna 5 frente a 1" w l, './build/datosFinales.txt' using 1:6 t "Datos columna 6 frente a 1" w l 
 _end_
--- p1/functions.cpp
+++ p1/functions.cpp
@@ -1,4 +1,3 @@
-//#include <cstdlib>
 #include <iostream>
 #include <ctime>
 #include <vector>
@@ -6,11 +5,14 @@
 #include <cmath>
 #include "functions.hpp"
 #include "ClaseTiempo.cpp"
+#include "sistemaEcuaciones.hpp"
 
 void menu() {
+    std::cout << std::endl;
     std::cout << "1: Método de ordenación por selección" << std::endl;
     std::cout << "2: cálculo del cuadrado de una matriz" << std::endl;
     std::cout << "3: Cálculo del término n-ésimo de la sucesión de Fibonacci" << std::endl;
+    std::cout << "Introduzca cualquier valor para salir" << std::endl;
 }
 
 void rellenarVector(std::vector<int> &v){
@@ -70,7 +72,8 @@ void tiemposOrdenacionSelección(int nMin, int nMax, int increment, int repetici
     }
 }
 
-void almacenarFichero(std::vector <double> &tiemposReales, std::vector <double> &numeroElementos){
+void almacenarFichero(const std::vector <double> &tiemposReales,
+                      const std::vector <double> &numeroElementos){
     std::ofstream file("tiempoReales.txt");
 
     if(!file){
@@ -78,9 +81,8 @@ void almacenarFichero(std::vector <double> &tiemposReales, std::vector <double> 
         return;
     }
 
-    for(int i = 0; i < numeroElementos.size() && i < tiemposReales.size(); i++){
-        file << numeroElementos[i] << " --> " << tiemposReales[i] << " microsegundos" << std::endl;
-    }
+    for(int i = 0; i < numeroElementos.size() && i < tiemposReales.size(); i++)
+        file << numeroElementos[i] << " " << tiemposReales[i] << std::endl;
 
     file.close();
 }
@@ -89,32 +91,114 @@ void ajusteCuadratico(const std::vector<double> &numeroElementos,
                       const std::vector<double> &tiemposReales,
                       std::vector<double> &a){
     
-    //TODO
+    std::vector<std::vector<double>> A(3, std::vector<double>(3));
+    std::vector<std::vector<double>> B(3, std::vector<double>(1));
+    std::vector<std::vector<double>> X(3, std::vector<double>(1));
+
+    for(int i = 0; i < A.size(); i++){
+        for(int j = i; j < i+A[i].size(); j++)
+            A[i][j-i] = sumatorio(numeroElementos, tiemposReales, j, 0);
+        
+        B[i][0] = sumatorio(numeroElementos, tiemposReales, i, 1);
+    }
+
+    resolverSistemaEcuaciones(A, B, 3, X);
+
+    for(int i = 0; i < X.size(); i++)
+        a.push_back(X[i][0]);
 }
 
-double sumatorio(std::vector <double> &n, 
-                 std::vector <double> &t, 
+double sumatorio(const std::vector <double> &n, 
+                 const std::vector <double> &t, 
                  int expN, int expT){
     double sum = 0;
-    for(int i = 0; i<n.size() && i<t.size(); i++){
+    
+    for(int i = 0; i<n.size() && i<t.size(); i++)
         sum += std::pow(n[i], expN) * std::pow(t[i], expT);
+
+    return sum;
+}
+
+void calcularTiemposEstimadosPolinomico(const std::vector <double> &numeroElementos,
+                                        const std::vector <double> &a,
+                                        std::vector <double> &tiemposEstimados){
+    //t(n) = a0 + a1*n + a2*n²
+    for(int i = 0; i < numeroElementos.size(); i++){
+        double t = a[0] + a[1]*numeroElementos[i] + a[2]*(numeroElementos[i]*numeroElementos[i]);
+        tiemposEstimados.push_back(t);
     }
+}
+
+void almacenarDatosFichero(const std::vector <double> &tiemposReales, 
+                           const std::vector <double> &numeroElementos,
+                           const std::vector <double> &tiemposEstimados){
+    
+    std::ofstream file("datosFinales.txt");
+
+    if(!file){
+        std::cout << "No se ha podido abrir el fichero." << std::endl;
+        return;
+    }
+
+    for(int i = 0; i < numeroElementos.size() && i < tiemposReales.size() && i < tiemposEstimados.size(); i++)
+        file << numeroElementos[i] << " " << tiemposReales[i] << " " << tiemposEstimados[i] << std::endl;
+
+    file.close();
+}
+
+double calcularTiempoEstimadoPolinomico(const double &n, std::vector <double> &a){
+    //t(n) = a0 + a1*n + a2*n²
+    double t = a[0] + a[1]*n + a[2]*(n*n);
+    return t;
 }
 
 void ordenacionSeleccion(){
     std::vector<double> tiemposReales;
     std::vector<double> numeroElementos;
-    tiemposOrdenacionSelección(1000, 5000, 100, 5, tiemposReales, numeroElementos);
+    std::vector<double> a;
+    std::vector<double> tiemposEstimados;
+    int nMin, nMax, increment, rep, n;
+    char option;
 
-    std::cout << "Vector tiempos (" << tiemposReales.size() << "): ";
-    for (double i : tiemposReales)
-        std::cout << i << " ";
-    std::cout << std::endl;
+    std::cout << "Introduzca el minimo de elementos: ";
+    std::cin >> nMin;
 
-    std::cout << "Vector elementos (" << numeroElementos.size() << "): ";
-    for (double i : numeroElementos)
-        std::cout << i << " ";
-    std::cout << std::endl;
+    std::cout << "Introduzca el maximo de elementos: ";
+    std::cin >> nMax;
 
+    std::cout << "Introduzca el incremento: ";
+    std::cin >> increment;
+
+    std::cout << "Introduzca el numero de repeticiones: ";
+    std::cin >> rep;
+
+    std::cout << "Calculando tiempos..." << std::endl;
+
+    tiemposOrdenacionSelección(nMin, nMax, increment, rep, tiemposReales, numeroElementos);
     almacenarFichero(tiemposReales, numeroElementos);
+    ajusteCuadratico(numeroElementos, tiemposReales, a);
+
+    std::cout << std::endl;
+    std::cout << "Incognitas: " << std::endl;
+    std::cout << "a0 = " << a[0] << std::endl;
+    std::cout << "a1 = " << a[1] << std::endl;
+    std::cout << "a2 = " << a[2] << std::endl;
+
+    calcularTiemposEstimadosPolinomico(numeroElementos, a, tiemposEstimados);
+    almacenarDatosFichero(tiemposReales, numeroElementos, tiemposEstimados);
+
+    std::cout << "Estimar tiempos para un determinado valor?" << std::endl;
+    std::cout << "Si (S)" << std::endl;
+    std::cout << "No (N)" << std::endl;
+
+    std::cin >> option;
+    std::tolower(option);
+
+    //TODO
+    // switch(option){
+    //     case 's':
+    //         std::cout << "Introduzca numero elementos: ";
+    //         std::cin >> n;
+    // }
+
 }
