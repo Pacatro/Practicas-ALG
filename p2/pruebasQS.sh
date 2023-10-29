8171b36faa018db5b058b16ba5c1193e46d8536f
Pacatro <paco.algar.15@gmail.com>
2023-10-29 18:37:02 +0100

    prueba

--- p2/pruebasQS.sh
+++ p2/pruebasQS.sh
@@ -4,10 +4,12 @@ echo "Ejecutando pruebas..."
 
 numeroMedianas=(3 5 9 15 25)
 numeroMinimo=(500 1000 5000 10000 50000)
+num=1
 
 for med in "${numeroMedianas[@]}"; do
     for n in "${numeroMinimo[@]}"; do
-        echo -e "\nPrueba para numeroMediana = $med y numeroMinimo = $n\n"
+        echo -e "\nPrueba numero $num, numeroMediana = $med y numeroMinimo = $n\n"
         ./p2 -m 1000000 -M 5000000 -i 100000 -r 3 -e "$med" -n "$n"
+        num=$((num+1))
     done
 done
