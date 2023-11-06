4156b1cf04678a0724fe9c1a27b6d4644cfbb330
Pacatro <paco.algar.15@gmail.com>
2023-11-06 20:11:27 +0100

    cambio

--- p2/ejemplo_gnuplot.sh
+++ p2/ejemplo_gnuplot.sh
@@ -6,6 +6,6 @@ set terminal postscript eps color
 set output "grafico.eps"
 set key right bottom
 set xlabel "Tamano ejemplar"
-set ylabel "Tiempo"
+set ylabel "Tiempos ({/Symbol m}s)"
 plot 'tiemposReales.txt' using 1:2 t "Tiempos QS1" w l, 'tiemposReales.txt' using 1:3 t "Tiempos QS2" w l, 'tiemposReales.txt' using 1:5 t "Datos columna 5 frente a 1" w l, 'tiemposReales.txt' using 1:6 t "Datos columna 6 frente a 1" w l 
 _end_
