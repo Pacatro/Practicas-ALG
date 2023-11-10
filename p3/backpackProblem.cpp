6b7c82ca2ed27e8c9e98db7a0e86c74f1a37abb2
Pacatro <paco.algar.15@gmail.com>
2023-11-10 13:22:19 +0100

    empezando backpack

--- p3/backpackProblem.cpp
+++ p3/backpackProblem.cpp
@@ -1,5 +1,11 @@
+#include <iostream>
+
 #include "headers/backpackProblem.hpp"
 
 void backpackProblem(){
+    int v;
+    std::cout << "Introduzca el volumen de la mochila: ";
+    std::cin >> v;
 
+    
 }
